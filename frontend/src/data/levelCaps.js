bdffcdb5b570e49227bcfb8667ec34e77c6f41d1
RedBunny9549 <125116292+RedBunny9549@users.noreply.github.com>
2026-04-01 22:19:04 -0400

    Create levelCaps.js

---
+++ frontend/src/data/levelCaps.js
@@ -0,0 +1,49 @@
+// frontend/src/data/levelCaps.js
+
+export const LEVEL_CAPS = {
+  emerald: {
+    "Roxanne": 15,
+    "Brawly": 19,
+    "Wattson": 24,
+    "Flannery": 29,
+    "Norman": 31,
+    "Winona": 33,
+    "Tate & Liza": 42,
+    "Juan": 46,
+    "Sidney": 49,
+    "Phoebe": 51,
+    "Glacia": 53,
+    "Drake": 55,
+    "Wallace": 58
+  },
+  firered: {
+    "Brock": 14,
+    "Misty": 21,
+    "Lt. Surge": 24,
+    "Erika": 29,
+    "Koga": 43,
+    "Sabrina": 43,
+    "Blaine": 47,
+    "Giovanni": 50,
+    "Lorelei": 54,
+    "Bruno": 58,
+    "Agatha": 58,
+    "Lance": 60,
+    "Blue": 65
+  },
+  crystal: {
+    "Falkner": 9,
+    "Bugsy": 16,
+    "Whitney": 20,
+    "Morty": 25,
+    "Chuck": 30,
+    "Jasmine": 35,
+    "Pryce": 31,
+    "Clair": 40,
+    "Will": 42,
+    "Koga": 44,
+    "Bruno": 46,
+    "Karen": 47,
+    "Lance": 50
+  }
+};
